56ba88f39a5db8b77010b9bb24a72d3989319b37
Jovane Maciel De Sousa <jovane.sousa@redeflex.com.br>
2025-11-13 15:39:11 -0400

    feat: adicionando config swagger para jwt

---
+++ fin-api/Configuration/SwaggerConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace fin_api.Configuration
+{
+    public static class SwaggerConfig
+    {
+
+        public static WebApplicationBuilder AddSwaggerConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            return builder;
+        }
+    }
+}
--- fin-api/Program.cs
+++ fin-api/Program.cs
@@ -6,10 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
-builder.Services.AddEndpointsApiExplorer();
+
 builder
+    .AddSwaggerConfig()
     .AddDbContextConfig()
     .AddIdentityConfig();
 
