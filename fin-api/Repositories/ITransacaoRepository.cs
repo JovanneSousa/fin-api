9989c7f9f5dcf8ee720ce37114193110a4d4d2bb
Jovane Maciel De Sousa <jovane.sousa@redeflex.com.br>
2025-11-14 16:36:13 -0400

    fix: adicionando interface que não foi salva

--- fin-api/Repositories/ITransacaoRepository.cs
+++ fin-api/Repositories/ITransacaoRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace fin_api.Repositories
+﻿using fin_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace fin_api.Repositories
 {
     public interface ITransacaoRepository
     {
+        public Task<Transacao> GetByIdAsync(string id);
+
+        public Task<IEnumerable<Transacao>> GetAllAsync(string userId);
+
+        public Task AddAsync(Transacao transaction);
+
+        public Task UpdateAsync(Transacao transaction);
+
+        public Task DeleteAsync(string id);
     }
 }
